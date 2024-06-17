87dc4f98bc46b7003d50b966fe54a8a75e51ae4c
Bloop <bloophuang@wondercise.com>
2024-06-17 17:22:31 +0800

    init project

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import App from '../App';
+
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+test('Renders the main page', () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
