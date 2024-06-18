60afa2ea4b9ca06574956c1eaf120a5e627e46dc
Bloop <bloophuang@wondercise.com>
2024-06-18 13:59:10 +0800

    feat: setup path aliases

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 export default {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  rootDir: "src",
+  rootDir: ".",
   moduleNameMapper: {
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/src/__mocks__/fileMock.js",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
   },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
--- src/components/Cart/__tests__/Cart.test.tsx
+++ src/components/Cart/__tests__/Cart.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 
 // import Cart from '../index';
 import Cart from "@/components/Cart";
